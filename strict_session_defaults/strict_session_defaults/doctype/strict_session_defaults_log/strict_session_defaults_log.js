4bbf9531de69c0224a51f97406a2ac9d2f579086
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-24 22:23:19 +0300

    Initial release

---
+++ strict_session_defaults/strict_session_defaults/doctype/strict_session_defaults_log/strict_session_defaults_log.js
@@ -0,0 +1,8 @@
+/*
+*  Frappe Strict Session Defaults © 2022
+*  Author:  Ameen Ahmed
+*  Company: Level Up Marketing & Software Development Services
+*  Licence: Please refer to license.txt
+*/
+
+frappe.ui.form.on('Strict Session Defaults Log', {});
