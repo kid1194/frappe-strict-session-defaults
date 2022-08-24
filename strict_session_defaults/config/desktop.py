301b9013600ac07820da88afe3bfb60ff0c0403f
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-24 21:56:05 +0300

    Initial release

---
+++ strict_session_defaults/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Strict Session Defaults",
+            "color": "blue",
+            "icon": "octicon octicon-unlock",
+            "type": "module",
+            "label": _("Strict Session Defaults")
+        }
+    ]
